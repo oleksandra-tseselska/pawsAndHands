3a0beca968f14b3d1fff78c146bfe0013dcab252
Oleksandra Tseselska <alexiyast@gmail.com>
2023-02-03 19:22:11 +0200

    first commit

---
+++ src/main/java/com/pawsandhands/PawsAndHandsApplication.java
@@ -0,0 +1,13 @@
+package com.pawsandhands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawsAndHandsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawsAndHandsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pawsandhands/PawsAndHandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawsandhands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawsAndHandsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
