d3e1b01cfbddeead40967d7a8e7902da931daac0
mpaklonskaya <mariapoklonskaya@gmail.com>
2023-02-04 10:26:09 +0200

    added fragments and tried to unify header

---
+++ src/main/java/com/pawsandhands/PetEntity/PetController.java
@@ -0,0 +1,43 @@
+package com.pawsandhands.PetEntity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PetController {
+
+//        private final UserService userService;
+
+//        @Autowired
+//        public UserController(UserService userService){
+//            this.userService = userService;
+//        }
+
+        @GetMapping("/create-pet")
+        public String showCreatePetPage(){
+            return "create-pet"; //will find register in the Template folder. When we want to display the page
+        }
+
+//        @PostMapping("/createPet")
+//        public String handlePetCreation(Pet pet){
+//            try{
+//                this.userService.createUser(user);
+//
+//            }
+//            catch(Exception e){
+//                return "redirect:/?message=signup_failed&error" + e.getMessage();
+//
+//            }
+//            return "redirect:/?message=signup_success"; //when we want to send user to another endpoint
+//        }
+//
+//        @GetMapping("/login")
+//        public String showLoginPage(
+//
+//        ){
+//            return "login"; //will find register in the Tempate folder. When we want to display the page
+//        }
+//    }
+
+
+}
