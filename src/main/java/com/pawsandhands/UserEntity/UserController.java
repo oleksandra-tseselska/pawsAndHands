8a9c80a0f4b70b2b16c40b6b1f1d5e9cbf9c9f38
Oleksandra Tseselska <alexiyast@gmail.com>
2023-02-05 11:09:44 +0200

    Add anchor to /allUsers

--- src/main/java/com/pawsandhands/UserEntity/UserController.java
+++ src/main/java/com/pawsandhands/UserEntity/UserController.java
@@ -2,8 +2,11 @@ package com.pawsandhands.UserEntity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -48,5 +51,53 @@ public class UserController {
 
     }
 
+    @GetMapping("/all-users")
+    public String showUsers(Model model){
+        System.out.println(this.userService.findAll());
+
+        try{
+            model.addAttribute("usersList", this.userService.findAll());
+        }catch (Exception e){
+            return "redirect:users?message=search_filed&error=" + e.getMessage();
+        }
+
+        return "all-users";
+    }
+
+    @GetMapping("/userUpdate/{userId}")
+    public String updateUser(@PathVariable Integer userId,
+                             Model model)
+    {
+        User user = userService.findById(userId);
+
+        try{
+            model.addAttribute("userData", userService.findById(userId));
+        }catch (Exception e){
+            return "redirect:user?message=search_filed&error=" + e.getMessage();
+        }
+
+        return "userUpdate";
+    }
+
+    @PostMapping("/userUpdateBtn")
+    public String updateUserData (@RequestParam(name = "userId", required = false) Integer userId,
+                                  @RequestParam(name = "firstName", required = false) String firstName,
+                                  @RequestParam(name = "password", required = false) String password){
+
+        User user = userService.findById(userId);
+        System.out.println(userId);
+        System.out.println(firstName);
+        System.out.println(password);
+        System.out.println(user.toString());
+
+        if(!user.getName().equals(firstName)){
+
+        }
+        if(!user.getPassword().equals(password)){
+
+        }
+
+        return "redirect:users";
+    }
 
 }
